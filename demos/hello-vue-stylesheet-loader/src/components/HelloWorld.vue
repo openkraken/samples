2f2fe80761f44cf0d846c430e335665a0bbefbcc
andycall <dongtiangche@outlook.com>
2022-06-19 23:09:03 +0800

    refactor: refactor helloworld.vue

--- demos/hello-vue-stylesheet-loader/src/components/HelloWorld.vue
+++ demos/hello-vue-stylesheet-loader/src/components/HelloWorld.vue
@@ -7,25 +7,7 @@
 </template>
 
 <script>
-const style = {
-  helloWorld: {
-    boxSizing: 'border-box',
-    display: 'flex',
-    position: 'relative',
-    flexDirection: 'column',
-    flexShrink: 0,
-    alignContent: 'flex-start',
-    borderWidth: 0,
-    borderStyle: 'solid',
-    borderColor: 'black',
-    margin: '2.66667vw 0vw',
-    padding: 0,
-    minWidth: 0,
-    alignItems: 'center',
-    fontSize: '6vw',
-    fontWeight: 'bold',
-  }
-}
+import style from './HelloWorld.inline.css';
 
 export default {
   name: 'HelloWorld',
